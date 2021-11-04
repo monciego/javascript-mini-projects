0b4bd4dd188d8762878a4c891da61905634a1464
Jericho <jerichobantiquete@gmail.com>
2021-11-05 00:46:24 +0800

    counter app

--- 01-counter/app.js
+++ 01-counter/app.js
@@ -1,6 +1,7 @@
 const increment = document.getElementById("increment");
 const decrement = document.getElementById("decrement");
 const reset = document.getElementById("reset");
+const circle = document.querySelector(".count-circle");
 
 let countEl = document.getElementById("count");
 
@@ -28,10 +29,13 @@ reset.addEventListener("click", () => {
 // style function
 const style = () => {
   if (count > 0) {
-    countEl.style.color = "green";
+    countEl.style.color = "#218380";
+    circle.style.border = "#218380 0.75rem solid";
   } else if (count < 0) {
-    countEl.style.color = "red";
+    countEl.style.color = "#D81159";
+    circle.style.border = "#D81159 0.75rem solid";
   } else {
-    countEl.style.color = "black";
+    countEl.style.color = "#0d0628";
+    circle.style.border = "#c643b7 0.75rem solid";
   }
 };
