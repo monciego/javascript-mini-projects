05d2c4818f1c80a8f3b5ecdbab76c8a30ae15ded
Jericho <jerichobantiquete@gmail.com>
2021-11-05 16:33:28 +0800

    added the filename in result and changed into table

--- 02-file-extension/app.js
+++ 02-file-extension/app.js
@@ -1,19 +1,30 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+const table = document.getElementById("table");
 const inputValue = document.getElementById("inputValue");
-const filename = document.getElementById("filename");
+const filename = document.getElementById("fileName");
+const error = document.getElementById("error");
 
 btn.addEventListener("click", () => {
   const value = input.value;
 
   if (value === "") {
-    inputValue.innerHTML = "PLEASE ENTER A FILE NAME";
+    error.innerHTML = "PLEASE ENTER A FILE NAME";
+    error.style.display = "block";
+    table.style.opacity = "0";
+    table.style.pointerEvents = "none";
   } else if (value.includes(".") === false) {
-    inputValue.innerHTML = "INVALID FILENAME";
+    error.innerHTML = "INVALID FILENAME";
+    error.style.display = "block";
+    table.style.opacity = "0";
+    table.style.pointerEvents = "none";
   } else {
     console.log(value.slice(value.lastIndexOf(".")));
-
+    table.style.opacity = "1";
+    table.style.pointerEvents = "all";
     finalValue = value.slice(value.lastIndexOf("."));
+    filename.innerHTML = value;
     inputValue.innerHTML = finalValue;
+    error.style.display = "none";
   }
 });
